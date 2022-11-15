7ea7a99997ca505afc9e0f5053dc45df3260fe9c
rahylei <julioc3sarbarc3nas@gmail.com>
2022-11-15 07:34:26 +0000

    component zoom, test implementation with livewire

---
+++ app/Http/Livewire/Zoom.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Zoom extends Component
+{
+
+    public $text;
+
+    public function mount($text) {
+        $this->text = $text;
+    }
+
+    public function zoomplus(){
+        switch($this->text){
+            case "text-sm":
+                $this->text = "text-lg";
+                break;
+
+            case "text-lg":
+                $this->text = "text-2xl";
+                break;
+            default:
+                $this->text = "text-2xl";
+        }
+    }
+
+    public function zoommenos(){
+        switch($this->text){
+            case "text-lg":
+                $this->text = "text-sm";
+                break;
+
+            case "text-2xl":
+                $this->text = "text-lg";
+                break;
+            default:
+                $this->text = "text-sm";
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.zoom');
+    }
+}
---
+++ resources/views/livewire/zoom.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <button wire:click="zoomplus" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full {{$text}} px-5 py-2.5 text-center mr-2 mb-2">zoom plus</button>
+
+    <button wire:click="zoommenos" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full {{$text}} px-5 py-2.5 text-center mr-2 mb-2">zoom menos</button>
+
+    <p class="{{$text}}">
+        <br/>
+        
+        <br/>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptate reiciendis pariatur commodi aliquid consequatur dolorem ipsam suscipit eveniet cumque consectetur impedit reprehenderit, repudiandae animi. Quod perspiciatis impedit cum animi.
+    </p>
+</div>
