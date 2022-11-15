02bdbe17c14d646f13837afffc9d5c9848d6259d
rahylei <julioc3sarbarc3nas@gmail.com>
2022-11-15 06:33:11 +0000

    install and config fowbite on layouts app and guest

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'public/css/flowbite.css', 
+                'public/js/flowbite.js'
             ],
             refresh: [
                 ...refreshPaths,
