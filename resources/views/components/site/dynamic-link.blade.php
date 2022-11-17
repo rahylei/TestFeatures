f766a0e434948d85ad293d7725b0baf4ad958b86
rahylei <julioc3sarbarc3nas@gmail.com>
2022-11-17 00:13:04 -0600

    Change on navbar and links to add dropdown

--- resources/views/components/site/dynamic-link.blade.php
+++ resources/views/components/site/dynamic-link.blade.php
@@ -1,23 +1,11 @@
 @forelse ($modules as $module)
-    
-    <x-jet-dropdown align="right" width="48" >
-        <x-slot name="trigger">            
-            <button class="flex text-sm border-2 border-transparent transition">
-                {{ __($module->name) }}
-            </button>
-        </x-slot>
 
-        <x-slot name="content">
-            {{-- <x-jet-nav-link class="hover:bg-white hover:text-white" href="{{ route($module->route, $module)}}" :active="request()->routeIs($module->route)">
-        {{ __($module->name) }}
-    </x-jet-nav-link> --}}
-            <li>uno</li>
-            <li>dos</li>
-            <li>tres</li>
-            <li>cuatro</li>
-            <li>cinco</li>
+    <x-site.drop-down class="hover:bg-white hover:text-black" :name="$module->name" >
+        <x-slot:content>
+            caracoles
         </x-slot>
-    </x-jet-dropdown>
+    </x-site.drop-down>
+
 @empty
 
 @endforelse
