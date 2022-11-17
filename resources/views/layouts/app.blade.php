04e3f29168763def4f5ceb4213bb02edc6fadfdf
rahylei <julioc3sarbarc3nas@gmail.com>
2022-11-16 21:32:05 -0600

    Make and configure routes and views to Admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,8 +20,8 @@
         <x-jet-banner />
 
         <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
+            {{-- @livewire('navigation-menu') --}}
+            <x-admin.nav-bar/>
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
