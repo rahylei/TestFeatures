d36940fb2d1ab5384549d94bbe7890e9930d438e
rahylei <julioc3sarbarc3nas@gmail.com>
2022-11-17 01:17:59 -0600

    Make and implementation of carousel

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,5 +1,6 @@
 <x-site.layout>
-    <x-slot name="main">
+    <x-slot:main>
+        <x-site.carousel/>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
