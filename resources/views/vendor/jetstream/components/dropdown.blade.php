6d9748a324c246a006e7f6957775fb9dbe1bee63
rahylei <julioc3sarbarc3nas@gmail.com>
2022-11-16 21:41:33 -0600

    Alter dropdown from jetst to work in public site

--- resources/views/vendor/jetstream/components/dropdown.blade.php
+++ resources/views/vendor/jetstream/components/dropdown.blade.php
@@ -1,4 +1,4 @@
-@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white', 'dropdownClasses' => ''])
+@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-2 px-4 bg-white', 'dropdownClasses' => ''])
 
 @php
 switch ($align) {
@@ -25,23 +25,25 @@ switch ($width) {
 }
 @endphp
 
-<div class="relative" x-data="{ open: false }" @click.away="open = false" @close.stop="open = false">
-    <div @click="open = ! open">
-        {{ $trigger }}
-    </div>
+<div class="flex items-center hover:bg-white hover:text-black text-white font-semibold">
+    <div class="relative " x-data="{ open: false }" @click.away="open = false" @close.stop="open = false">
+        <div @click="open = ! open" class="">
+            {{ $trigger }}
+        </div>
 
-    <div x-show="open"
-            x-transition:enter="transition ease-out duration-200"
-            x-transition:enter-start="transform opacity-0 scale-95"
-            x-transition:enter-end="transform opacity-100 scale-100"
-            x-transition:leave="transition ease-in duration-75"
-            x-transition:leave-start="transform opacity-100 scale-100"
-            x-transition:leave-end="transform opacity-0 scale-95"
-            class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }} {{ $dropdownClasses }}"
-            style="display: none;"
-            @click="open = false">
-        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
-            {{ $content }}
+        <div x-show="open"
+                x-transition:enter="transition ease-out duration-200"
+                x-transition:enter-start="transform opacity-0 scale-95"
+                x-transition:enter-end="transform opacity-100 scale-100"
+                x-transition:leave="transition ease-in duration-75"
+                x-transition:leave-start="transform opacity-100 scale-100"
+                x-transition:leave-end="transform opacity-0 scale-95"
+                class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }} {{ $dropdownClasses }}"
+                style="display: none;"
+                @click="open = false">
+            <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
+                {{ $content }}
+            </div>
         </div>
     </div>
 </div>
