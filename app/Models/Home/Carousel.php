570ffec3893e0aa262af69d474a0e910049bd7ff
rahylei <julioc3sarbarc3nas@gmail.com>
2022-11-17 20:27:57 -0600

    Model and migration to Carousel

---
+++ app/Models/Home/Carousel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Home;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Carousel extends Model
+{
+    use HasFactory;
+    protected $guarded = [""];
+}
---
+++ database/migrations/2022_11_17_235027_create_carousels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carousels', function (Blueprint $table) {
+            $table->id();
+            $table->text('storage');
+            $table->text('url');
+            $table->boolean('active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carousels');
+    }
+};
